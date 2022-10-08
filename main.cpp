08e99a76764d30fc71762fe01b6a8ac2c431c923
Aditya Faiz <68331684+angin2791@users.noreply.github.com>
2022-10-08 09:17:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,26 +11,26 @@ using namespace std;
 #define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
 #define SUDUT 45 //sudut elevasi tembakan
 
-int mencari_V0(int input, int losses)
+int mencari_V0(int v, int losses)
 {
-	input -= losses;
-  	return input;
+	v -= losses;
+  	return v;
 }
 
-int speed_dgn_loss(int input)
+int speed_dgn_loss(int v)
 {
     int losses;
-	if (input >= 1 && input <= 10)
+	if (v >= 1 && v <= 10)
     {
         losses = 1;
     }
 
-    else if (input >= 11 && input <= 20)
+    else if (v >= 11 && v <= 20)
     {
         losses = 3;
     }
 
-    else if (input >= 21 && input <= 30)
+    else if (v >= 21 && v <= 30)
     {
         losses = 5;
     }
@@ -38,12 +38,12 @@ int speed_dgn_loss(int input)
 }
 
 int main() {
-    int input;
+    int v;
     float jarak, Vtangensial;
-  	cin >> input;
-    jarak = (pow(mencari_V0(input,speed_dgn_loss(input)),2)* sin(SUDUT*3.14159*2/180) / GRAVITASI) - START_PENGUKURAN;
+  	cin >> v;
+    jarak = (pow(mencari_V0(v,speed_dgn_loss(v)),2)* sin(SUDUT*3.14159*2/180) / GRAVITASI) - START_PENGUKURAN;
     jarak = round(jarak);
-    Vtangensial = sqrt(jarak * GRAVITASI / sin(SUDUT*3.14159*2/180)) + speed_dgn_loss(input);
+    Vtangensial = sqrt(jarak * GRAVITASI / sin(SUDUT*3.14159*2/180)) + speed_dgn_loss(v);
   	cout << jarak << " " << Vtangensial << endl;
     return 0;
 }
